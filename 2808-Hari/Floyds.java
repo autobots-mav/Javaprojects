740b27feac33ad0f40f9d207a1a814d7b004cc72
autobots-mav <57139475+autobots-mav@users.noreply.github.com>
2019-12-03 12:35:00 +0530

    Add files via upload

---
+++ 2808-Hari/Floyds.java
@@ -0,0 +1,17 @@
+package Learning;
+
+public class Floyds {
+	public static void main(String[] args) {
+		int n= 1;
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(n+" ");
+				n++;
+			}
+			System.out.println();
+		}
+	}
+
+}
